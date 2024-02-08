2b8def0d6629e815dad822a5590128ddc21b757a
charlene <charlene.xu92@gmail.com>
2024-02-07 18:07:57 -0800

    Update import path from Components to components and Pages to pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import { Outlet } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react'
-import Header from './Components/Header';
+import { Outlet } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 function App() {
   return (
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import SignUp from './Pages/SignUp';
-import LogIn from './Pages/LogIn';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: < App/>,
+    element: <App />,
     children: [
       {
         path: "/sign-up",
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(<RouterProvider router={router} />);

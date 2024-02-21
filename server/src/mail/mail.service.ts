1dd3ff3352bddba045d33a7d14e48ea5856e2bd0
charlene <charlene.xu92@gmail.com>
2024-02-20 16:55:10 -0800

    Send password reset email with Nodemailer

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
+import { sendMail } from './mail';
+import { resetPasswordTemplate } from './reset-password-template';
 
 @Injectable()
 export class MailService {
   async sendPasswordResetEmail(user: User, token: string) {
-    console.log('USER: ', user);
     console.log('TOKEN: ', token);
+    sendMail(
+      {
+        from: 'charlenexu120@gmail.com',
+        to: user.email,
+        subject: 'Reset Your Password',
+        html: resetPasswordTemplate(token, user.id),
+      },
+      () => {
+        console.log('password reset email sent');
+      },
+    );
     return 'HI';
   }
 }
